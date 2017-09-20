ecb1fe18d6ca9ee31848f8b70ea8e2061d788540
justinbushy <justinmbushlsphotography@gmail.com>
2017-09-20 11:41:22 -0600

    Better Test. 2 Tests Pass

--- src/test/java/com/example/calendarapi/UserControllerTest.java
+++ src/test/java/com/example/calendarapi/UserControllerTest.java
@@ -12,11 +12,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.assertj.core.api.Assertions.*;
-//import static org.mockito.BDDMockito.*;
+import static org.mockito.BDDMockito.*;
+import static org.hamcrest.Matchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
@@ -40,7 +42,14 @@ public class UserControllerTest {
 
 		mvc.perform(put("/register")
                 .contentType(MediaType.APPLICATION_JSON).content(json))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName", is("Jordan")))
+                .andExpect(jsonPath("$.lastName", is("Bush")))
+                .andExpect(jsonPath("$.email", is("jbush@gmail.com")))
+                .andExpect(jsonPath("$.userName", is("jordanbush")));
+
+
+		given(userDao.findByFirstNameAndLastName("Jordan", "Bush")).willReturn(user);
     }
 
     @Test
